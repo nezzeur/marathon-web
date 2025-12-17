bfffab9bb05d6326af4cfd9753b3a64266373a4d
Quentin Baillet <quentin_baillet@ens.univ-artois.fr>
2025-12-17 20:11:16 +0100

    migration initiale

--- database/migrations/2026_11_29_102802_create_article_table.php
+++ database/migrations/2026_11_29_102802_create_article_table.php
@@ -14,22 +14,22 @@ return new class extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table->string('titre');
-            $table->text('resume')->default('');
-            $table->text('texte')->default('');
-            $table->string('image')->default('');
-            $table->string('media')->default('');
-	        $table->boolean("en_ligne")->default(false);
+            $table->text('resume');
+            $table->text('texte');
+            $table->string('image');
+            $table->string('media');
+            $table->boolean("en_ligne");
             $table->integer('nb_vues')->default(0);
             $table->foreignIdFor(\App\Models\User::class)->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
-            $table->foreignIdFor(\App\Models\Rythme::class)->nullable()->constrained()
+            $table->foreignIdFor(\App\Models\Rythme::class)->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
-            $table->foreignIdFor(\App\Models\Accessibilite::class)->nullable()->constrained()
+            $table->foreignIdFor(\App\Models\Accessibilite::class)->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
-            $table->foreignIdFor(\App\Models\Conclusion::class)->nullable()->constrained()
+            $table->foreignIdFor(\App\Models\Conclusion::class)->constrained()
                 ->onUpdate('cascade')
                 ->onDelete('cascade');
             $table->timestamps();
