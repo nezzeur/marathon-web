121b6cda75580b3ae2430173ce68a44ae1f302d4
Quentin Baillet <quentin_baillet@ens.univ-artois.fr>
2025-12-17 13:26:47 +0100

    Ref #12 ajout des routes pour create et store avec la correction

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ArticleController::class, 'index'])->name("accueil");
 
-Route::get('/articles/{article}', [ArticleController::class, 'show'])->name("articles.show");
 Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
 Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+
+Route::get('/articles/{article}', [ArticleController::class, 'show'])->name("articles.show");
 
 // Routes pour filtrer les articles par caractéristique
 Route::get('/articles/accessibilite/{accessibilite}', [ArticleController::class, 'byAccessibilite'])->name("articles.byAccessibilite");
